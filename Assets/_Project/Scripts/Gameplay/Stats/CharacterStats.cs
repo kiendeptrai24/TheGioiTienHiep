0ad6912d1f692dfbb232b99ea6f176bf36d2553e
kiendeptrai24 <kienmiu2004@gmail.com>
2025-12-09 14:37:38 +0700

    refactor(stats): add SOStats folder and move SO preset files into it

--- Assets/_Project/Scripts/Gameplay/Stats/CharacterStats.cs
+++ Assets/_Project/Scripts/Gameplay/Stats/CharacterStats.cs
@@ -1,10 +1,6 @@
-
-
 using System;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.PlayerLoop;
 
 public class CharacterStats : TGTHNetworkBehaviour
 {
@@ -17,23 +13,23 @@ public class CharacterStats : TGTHNetworkBehaviour
     private Dictionary<StatType, Stat> stats = new Dictionary<StatType, Stat>();
 
     // Ví dụ expose property cho dễ gọi
-    public float Health => GetStatValue(StatType.Health);
-    public float Mana => GetStatValue(StatType.Mana);
-    public float Spirit => GetStatValue(StatType.Spirit);
+    public int Health => GetStatValue(StatType.Health);
+    public int Mana => GetStatValue(StatType.Mana);
+    public int Spirit => GetStatValue(StatType.Spirit);
 
-    public float PhysicalDamage => GetStatValue(StatType.PhysicalDamage);
-    public float MagicalDamage => GetStatValue(StatType.MagicalDamage);
-    public float SpiritDamage => GetStatValue(StatType.SpiritDamage);
+    public int PhysicalDamage => GetStatValue(StatType.PhysicalDamage);
+    public int MagicalDamage => GetStatValue(StatType.MagicalDamage);
+    public int SpiritDamage => GetStatValue(StatType.SpiritDamage);
 
-    public float PhysicalDefense => GetStatValue(StatType.PhysicalDefense);
-    public float MagicalDefense => GetStatValue(StatType.MagicalDefense);
-    public float SpiritDefense => GetStatValue(StatType.SpiritDefense);
+    public int PhysicalDefense => GetStatValue(StatType.PhysicalDefense);
+    public int MagicalDefense => GetStatValue(StatType.MagicalDefense);
+    public int SpiritDefense => GetStatValue(StatType.SpiritDefense);
 
-    public float MovementSpeed => GetStatValue(StatType.MovementSpeed);
-    public float AttackSpeed => GetStatValue(StatType.AttackSpeed);
-    public float CastSpeed => GetStatValue(StatType.CastSpeed);
+    public int MovementSpeed => GetStatValue(StatType.MovementSpeed);
+    public int AttackSpeed => GetStatValue(StatType.AttackSpeed);
+    public int CastSpeed => GetStatValue(StatType.CastSpeed);
 
-    public float CombatPower => GetStatValue(StatType.CombatPower);
+    public int CombatPower => GetStatValue(StatType.CombatPower);
     protected override void Awake()
     {
         InitStatsPreset();
@@ -57,18 +53,18 @@ public class CharacterStats : TGTHNetworkBehaviour
     private void AddStatsFromPreset()
     {
         statsRealmPreset.ApplyStats(stats);
-        statsRacePreset.ApplyStats(stats);
         StatsCultivationPathPreset.ApplyStats(stats);
+        statsRacePreset.ApplyStats(stats);
     }
-    public float GetStatValue(StatType type)
+    public int GetStatValue(StatType type)
     {
         if (stats.TryGetValue(type, out Stat stat))
         {
-            return stat.GetValue();
+            return Mathf.RoundToInt(stat.GetValue());
         }
 
         Debug.LogWarning($"Stat {type} không tồn tại trên {name}!");
-        return 0f;
+        return 0;
     }
     protected override void Start()
     {
