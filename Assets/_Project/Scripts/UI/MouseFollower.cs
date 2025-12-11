1de2e5ea73f4ca04f59b9adb6880371ffb28e8f2
kiendeptrai24 <kienmiu2004@gmail.com>
2025-12-11 15:31:43 +0700

    feat(inventory): implement inventory UI and item interactions

---
+++ Assets/_Project/Scripts/UI/MouseFollower.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class MouseFollower : MonoBehaviour
+{
+    [SerializeField]
+    private Canvas canvas;
+    [SerializeField]
+    private InputManager inputManager;
+
+    [SerializeField]
+    private UIInventoryItem item;
+
+    public void Awake()
+    {
+        canvas = transform.root.GetComponent<Canvas>();
+        item = GetComponentInChildren<UIInventoryItem>();
+    }
+
+    public void SetData(Sprite sprite, int quantity)
+    {
+        item.SetData(sprite, quantity);
+    }
+    void Update()
+    {
+        if(inputManager  == null) return;
+        Vector2 position;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            (RectTransform)canvas.transform,
+            inputManager.GetUIInputDirection(),
+            canvas.worldCamera,
+            out position
+                );
+        transform.position = canvas.transform.TransformPoint(position);
+    }
+    
+    public void Toggle(bool val)
+    {
+        Debug.Log($"Item toggled {val}");
+        gameObject.SetActive(val);
+    }
+}
---
+++ Assets/_Project/Scripts/UI/UIInventoryItem.cs
@@ -0,0 +1,95 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+using UnityEngine.EventSystems;
+
+    public class UIInventoryItem : MonoBehaviour, IPointerClickHandler,
+        IBeginDragHandler, IEndDragHandler, IDropHandler, IDragHandler
+    {
+        [SerializeField]
+        private Image itemImage;
+        [SerializeField]
+        private TMP_Text quantityTxt;
+
+        [SerializeField]
+        private Image borderImage;
+
+        public event Action<UIInventoryItem> OnItemClicked,
+            OnItemDroppedOn, OnItemBeginDrag, OnItemEndDrag,
+            OnRightMouseBtnClick;
+
+        private bool empty = true;
+        public Item item;
+        public void Awake()
+        {
+            ResetData();
+            Deselect();
+        }
+        public void ResetData()
+        {
+            item = null;
+            itemImage.gameObject.SetActive(false);
+            empty = true;
+        }
+        public void Deselect()
+        {
+            borderImage.enabled = false;
+        }
+        public void SetItem(Item newItem)
+        {
+            item = newItem;
+            if(item == null)
+            {
+                ResetData();
+                return;
+            }
+            SetData(item.itemIcon, item.maxStack);
+        }
+        public void SetData(Sprite sprite, int quantity)
+        {
+            itemImage.gameObject.SetActive(true);
+            itemImage.sprite = sprite;
+            quantityTxt.text = quantity + "";
+            empty = false;
+        }
+
+        public void Select()
+        {
+            borderImage.enabled = true;
+        }
+
+        public void OnPointerClick(PointerEventData pointerData)
+        {
+            if (pointerData.button == PointerEventData.InputButton.Right)
+            {
+                OnRightMouseBtnClick?.Invoke(this);
+            }
+            else
+            {
+                OnItemClicked?.Invoke(this);
+            }
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            OnItemEndDrag?.Invoke(this);
+        }
+
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            if (empty)
+                return;
+            OnItemBeginDrag?.Invoke(this);
+        }
+
+        public void OnDrop(PointerEventData eventData)
+        {
+            OnItemDroppedOn?.Invoke(this);
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            Debug.Log("Drag");
+        }
+    }
